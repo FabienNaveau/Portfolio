df7152844fc567b51c16804149486afa8f90a4e8
FabienNaveau <fabien.naveau@gmail.com>
2021-11-25 17:00:57 +0100

    Ajout de Bulma en CSS et header responsive + desktop fini

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
+
 import PhotoProfil from "../../img/PhotoProfil.jpg"
 import "./index.css"
 
 export default function Content() {
-    const description = "Bonjour, je suis Fabien Naveau, 30 ans développeur fullstack Javascript. Fraîchement sorti de formation, je suis à la recherche d'un Contrat à Durée Indeterminée afin de parfaire mes connaissances et monter en compétences.";
-    let x = 0;
-    let result = "";
-    const descriptionSplit = description.split("");
-    const descriptionInterval = () => {
-        setInterval(() => {
-            if (x < descriptionSplit.length) {
-                const paragraph = document.querySelector(".content--paragraph")
-                result += descriptionSplit[x]
-                paragraph.textContent = result;
-                x++;
-            } else return
-        }, 30)
-    }
-
-    descriptionInterval()
+    const firstParagraph = "Bonjour, je suis Fabien Naveau, 30 ans développeur fullstack Javascript. Fraîchement sorti de formation, je suis à la recherche d'un Contrat à Durée Indeterminée afin de parfaire mes connaissances et monter en compétences.";
+    const secondParagraph = "Durant cette formation j'ai pu, pendant les 3 premiers mois, acquerir des connaissances en HTML, CSS, JavaScript, NodeJS, PostgreSQL. Ensuite j'ai poursuivi pendant 1 mois avec la spécialisation Data/API, où nous voyons des notions plus poussées en NodeJS/Express, SQL,mais également des outils de versionning de BDD comme Sqitch et des initiations à graphQL, MongoDB, PGAdmin, Redis. J'ai enfin terminé par 1 mois de projet en totale autonomie en compagnie de 5 élèves de la promo. J'ai pu alors parfaire mes connaissances mais également découvrir la gestion de projet de A à Z et travailler en méthode agile (daily scrum, backlogs, userstories, sprints, Trello)."
+    
     return (
-        <main id="main">
-            <img className="content--img" src={PhotoProfil} />
-            <p className="content--paragraph"></p>
-        </main>
+        <section id="content">
+            <div className="content--imgbox">
+                <img className="content--imgbox__img" src={PhotoProfil} />
+            </div>
+            <div className="content--p">
+                <p className="content--paragraph">{firstParagraph}</p><br />
+                <p className="content--paragraph">{secondParagraph}</p>
+            </div>
+            <div>
+            <span className="icon-text has-text-info">
+                <span className="icon">
+                    <i className="fas fa-info-circle"></i>
+                </span>
+                <span>Info</span>
+            </span>
+            </div>
+        </section>
 
     )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,47 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import "./index.css"
 import { NavLink } from 'react-router-dom'
 import Logo from "../../img/LogoPortfolio.png"
 
-export default function Header () {
-    return(
+function toggleNavbar(event) {
+    const menuElm = document.querySelector(".navbar-menu");
+    menuElm.classList.toggle("is-active")
+    event.currentTarget.classList.toggle("is-active")
+}
+
+export default function Header() {
+    return (
         <header id="header">
-            <div className="header--boximg">
-                <img className="header--boximg__img" src={Logo} alt="Logo du portfolio" />
-            </div>
-            <nav className="header--nav">
-                <NavLink to="#projets">Projets</NavLink>
-                <NavLink to="#competences">Compétences</NavLink>
-                <NavLink to="#contact">Me contacter</NavLink>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+
+                    <a onClick={toggleNavbar} role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                    <a href="#presentation" class="navbar-item">
+                            Présentation
+                        </a>
+
+                        <a href="#projets" class="navbar-item">
+                            Projets
+                        </a>
+
+                        <a href="#competences" class="navbar-item">
+                            Compétences
+                        </a>
+
+                        <a href="#contact" class="navbar-item">
+                            Contact
+                        </a>
+                    </div>
+                </div>
             </nav>
         </header>
-    )
+                    )
 }
