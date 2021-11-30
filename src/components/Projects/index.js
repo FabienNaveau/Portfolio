b374164d8b478aae699b3cb5785f40f09a3468d8
FabienNaveau <fabien.naveau@gmail.com>
2021-11-30 16:42:20 +0100

    Component Projects desktop done

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -24,9 +24,9 @@ const Projects = () => {
                     </div>
                     <p className="card--content__paragraph">Kidozanges est une plateforme permettant à des parents de trouver des activités près de chez eux pour leurs enfants. Cette application participative permet également aux parents de soumettre les activités qu'ils connaissent afin d'enrichir le site. Conception de la partie back-end de l'application.</p>
                     <div className="card--content__link">
-                        <a href="https://kidozanges.netlify.app" target="_blank">Lien vers le site</a>
-                        <a href="#">Github back</a>
-                        <a href="#">Github front</a>
+                        <a href="https://kidozanges.netlify.app" target="_blank"><i class="fas fa-link"></i> Lien vers le site</a>
+                        <a href="#"><i class="fab fa-github"></i> Github back</a>
+                        <a href="#"><i class="fab fa-github"></i> Github front</a>
                     </div>
                 </div>
             </div>
@@ -43,8 +43,8 @@ const Projects = () => {
                     </div>
                     <p className="card--content__paragraph">Conception de mon site afin de présenter mes projets et apprendre React.</p>
                     <div className="card--content__link">
-                        <a href="#">Lien vers le site</a>
-                        <a href="https://github.com/FabienNaveau/Portfolio" target="_blank">Github</a>
+                        <a href="#"><i class="fas fa-link"></i> Lien vers le site</a>
+                        <a href="https://github.com/FabienNaveau/Portfolio" target="_blank"><i class="fab fa-github"></i> Github</a>
                     </div>
                 </div>
             </div>
